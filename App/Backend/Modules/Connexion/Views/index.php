ad5db4701ddd030133a52c625767822051781c45
sebastien <sebastienfolliot@yahoo.fr>
2018-04-20 14:59:58 +0200

    Personnalisation du formulaire sur le fichier connexion.php

--- App/Backend/Modules/Connexion/Views/index.php
+++ App/Backend/Modules/Connexion/Views/index.php
@@ -1,11 +1,16 @@
-<h2>Connexion</h2>
- 
-<form action="" method="post">
-  <label>Pseudo</label>
-  <input type="text" name="login" /><br />
- 
-  <label>Mot de passe</label>
-  <input type="password" name="password" /><br /><br />
- 
-  <input type="submit" value="Connexion" />
-</form>
+<div class="container">
+    <section class="col-sm-8 col-md-12">
+        <form class="well" action="" method="post">
+            <legend>
+                <h2>Connexion à l'interface administrateur</h2>
+            </legend>
+            <fieldset>
+                <label>Login :</label><br />
+                <input type="text" name="login" /><br /><br />
+                <label>Mot de passe :</label><br />
+                <input type="password" name="password" /><br /><br />
+                <button class="btn" type="submit"><span class="glyphicon glyphicon-ok-sign"></span> SE CONNECTER</button>
+            </fieldset>
+        </form>
+    </section>
+</div>

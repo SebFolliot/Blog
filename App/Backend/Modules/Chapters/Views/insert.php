250243f0880e284e126800ec8df8286c075665f9
sebastien <sebastienfolliot@yahoo.fr>
2018-04-20 14:50:21 +0200

    Modification de la structure du formulaire de la page insert.php

--- App/Backend/Modules/Chapters/Views/insert.php
+++ App/Backend/Modules/Chapters/Views/insert.php
@@ -1,8 +1,13 @@
-<h2>Ajouter un chapitre</h2>
-<form action="" method="post">
-  <p>
-    <?= $form ?>
-    
-    <input type="submit" value="Ajouter" />
-  </p>
-</form>
+<div class="container">
+    <section class="col-sm-8 col-md-12">
+        <form class="well" action="" method="post">
+            <legend>
+                <h2>Ajouter un chapitre</h2>
+            </legend>
+            <fieldset>
+                <?= $form ?>
+                <button class="btn" type="submit"><span class="glyphicon glyphicon-ok-sign"></span> VALIDER</button>
+            </fieldset>
+        </form>
+    </section>
+</div>

9cc1ebffd3bbcd8dbbc3bfbccbba1c25a23f8c64
sebastien <sebastienfolliot@yahoo.fr>
2018-04-20 15:18:41 +0200

    Création du fichier disconnect.php

---
+++ Web/disconnect.php
@@ -0,0 +1,20 @@
+<?php
+
+// On annule toutes les variables de session
+$_SESSION = array();
+
+// Suppression des cookies de session afin de détruire la session
+
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
+
+// On détruit la session
+session_destroy();
+
+
+header('Location: http://monsite');

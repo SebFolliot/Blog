c4b88cf355d1de062959c6c459cd8fd6c73bab98
sebastien <sebastienfolliot@yahoo.fr>
2018-04-26 10:49:51 +0200

    Ajout d'un script avec message temporaire

--- App/Frontend/Modules/Chapters/Views/index.php
+++ App/Frontend/Modules/Chapters/Views/index.php
@@ -1,3 +1,18 @@
+<style>
+    footer {
+        position: relative;
+    }
+</style>
+<!-- script permettant d'afficher temporairement le message -->
+<script>
+    document.getElementById("info_report").innerHTML = "Votre signalement est bien remonté auprès du modérateur !";
+
+    setTimeout(function() {
+        document.getElementById("info_report").innerHTML = "";
+    }, 3000);
+</script>
+
+
 <section style="background: url('../images/train_alaska.jpg') no-repeat center; height:563px; color:white; margin-bottom: 20px">
     <div style="text-align:center">
         <p style="font-size:2em"><em>Jean Forteroche</em></p>
@@ -5,15 +20,15 @@
     </div>
 </section>
 
-    <aside class="col-md-8; col-sm-10; col-xs-12" style="text-align: center; margin-bottom: 20px; font-size: 20px">
-        <div class="img-thumbnail" style="text-align: left; background-color: rgba(255,255,255,0.3)">
-            <p style="text-align: center; font-size: 30px">Bonjour et bienvenue sur mon blog</p>
-            <p>Retrouvez mon tout nouveau roman <strong>Billet simple pour l'Alaska</strong> que je diffuse chapitre par chapitre.<br></p>
-            <p>Bonne lecture.<br>
-               <em>Jean Forteroche</em>
-            </p>
-        </div>
-    </aside>
+<aside class="col-md-8; col-sm-10; col-xs-12" style="text-align: center; margin-bottom: 20px; font-size: 20px">
+    <div class="img-thumbnail" style="text-align: left; background-color: rgba(255,255,255,0.3)">
+        <p style="text-align: center; font-size: 30px">Bonjour et bienvenue sur mon blog</p>
+        <p>Retrouvez mon tout nouveau roman <strong>Billet simple pour l'Alaska</strong> que je diffuse chapitre par chapitre.<br></p>
+        <p>Bonne lecture.<br>
+            <em>Jean Forteroche</em>
+        </p>
+    </div>
+</aside>
 
 <?php
 
@@ -27,12 +42,10 @@ foreach ($listeChapters as $chapters)
             </a>
         </h2>
 
-        <p class="row col-md-12" style="border-bottom: 2px silver dashed; padding-bottom: 30px;">
+        <p class="row col-md-12" style="padding-bottom: 30px">
             <?= nl2br($chapters['contenu']) ?> <a href="chapters-<?= $chapters['id'] ?>.html"><em>Lire la suite / Commentaire</em></a>
         </p>
-
     </div>
     <?php
 }
-
 ?>

90dd5ad6a4bbfbd3ff9728f922cad0f00df817ed
sebastien <sebastienfolliot@yahoo.fr>
2018-04-26 11:49:46 +0200

    Ajout de style aux balises input

--- App/Backend/Modules/Connexion/Views/index.php
+++ App/Backend/Modules/Connexion/Views/index.php
@@ -1,4 +1,5 @@
 <div class="container">
+    
     <section class="col-sm-8 col-md-12">
         <form class="well" action="" method="post">
             <legend>
@@ -6,11 +7,14 @@
             </legend>
             <fieldset>
                 <label>Login :</label><br />
-                <input type="text" name="login" /><br /><br />
+                <input type="text" name="login" style="border-color: #397385" /><br /><br />
                 <label>Mot de passe :</label><br />
-                <input type="password" name="password" /><br /><br />
+                <input type="password" name="password" style="border-color: #397385" /><br /><br />
                 <button class="btn" type="submit"><span class="glyphicon glyphicon-ok-sign"></span> SE CONNECTER</button>
             </fieldset>
         </form>
     </section>
+    
 </div>
+
+

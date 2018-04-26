635825872f6bac3d33694a4010272fed5613294a
sebastien <sebastienfolliot@yahoo.fr>
2018-04-26 11:16:45 +0200

    Ajout de commentaire à la méthode run

--- App/Frontend/FrontendApplication.php
+++ App/Frontend/FrontendApplication.php
@@ -14,10 +14,13 @@ class FrontendApplication extends Application
 
   public function run()
   {
+      // Obtention du contrôleur
     $controller = $this->getController();
+      // Exécution du contrôleur
     $controller->execute();
-
+      // La page créée par le contrôleur est assignée à la réponse
     $this->httpResponse->setPage($controller->page());
+      // Envoi de la réponse
     $this->httpResponse->send();
   }
 }

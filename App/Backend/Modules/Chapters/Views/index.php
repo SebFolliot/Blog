90732fc3c3d4f67c9ff59e0d6b92ec2ab29f553d
sebastien <sebastienfolliot@yahoo.fr>
2018-04-26 11:38:08 +0200

    Mise en forme du tableau html

--- App/Backend/Modules/Chapters/Views/index.php
+++ App/Backend/Modules/Chapters/Views/index.php
@@ -1,11 +1,32 @@
-<p style="text-align: center">Il y a actuellement <?= $nombreChapters ?> chapitre(s). En voici la liste :</p>
- 
-<table>
-  <tr><th>Auteur</th><th>Titre</th><th>Date d'ajout</th><th>Dernière modification</th><th>Action</th></tr>
-<?php
-foreach ($listeChapters as $chapters)
-{
-  echo '<tr><td>', $chapters['auteur'], '</td><td>', $chapters['titre'], '</td><td>le ', $chapters['dateAjout']->format('d/m/Y à H\hi'), '</td><td>', ($chapters['dateAjout'] == $chapters['dateModif'] ? '-' : 'le '.$chapters['dateModif']->format('d/m/Y à H\hi')), '</td><td><a href="chapters-update-', $chapters['id'], '.html"><img src="/images/update.png" alt="Modifier" /></a> <a href="chapters-delete-', $chapters['id'], '.html"><img src="/images/delete.png" alt="Supprimer" /></a></td></tr>', "\n";
-}
-?>
+<!-- page index backend -->
+<table class="table table-striped table-condensed">
+    
+    <div class="card-header" style="background-color:silver; color:white">
+        <h2 class="card-title" style="text-align: center">
+            <?php if ($nombreChapters > 0) { 
+                    echo 'Gestion des ' .$nombreChapters. ' chapitres';
+                  }
+                  else { 
+                      echo 'Il n\'y a pas de chapitre pour le moment';
+                  } ?> </h2>
+    </div>
+    <thead>
+        <tr>
+            <th>Auteur</th>
+            <th>Titre</th>
+            <th>Date d'ajout</th>
+            <th>Dernière modification</th>
+            <th>Action</th>
+        </tr>
+    </thead>
+    <tbody>
+
+        <?php
+        foreach ($listeChapters as $chapters)
+        {
+          echo '<tr><td>', $chapters['auteur'], '</td><td>', $chapters['titre'], '</td><td>', $chapters['dateAjout']->format('d/m/Y à H\hi'), '</td><td>', ($chapters['dateAjout'] == $chapters['dateModif'] ? '-' :  $chapters['dateModif']->format('d/m/Y à H\hi')), '</td><td><a href="chapters-update-', $chapters['id'], '.html"><i class="fas fa-edit" title="Modifier"></i></a> <a href="chapters-delete-', $chapters['id'], '.html"><i class="fas fa-eraser" title="Supprimer"></i></a></td></tr>', "\n";
+        }
+        ?>
+    </tbody>
+    
 </table>

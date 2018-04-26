d437a090f405397390f9561baa3529979cfd9d40
sebastien <sebastienfolliot@yahoo.fr>
2018-04-26 11:42:05 +0200

    Mise en forme du formulaire

--- App/Backend/Modules/Chapters/Views/updateComment.php
+++ App/Backend/Modules/Chapters/Views/updateComment.php
@@ -1,8 +1,15 @@
-<h2>Modifier un commentaire</h2>
-<form action="" method="post">
-  <p>
-    <?= $form ?>
+<div class="container">
     
-    <input type="submit" value="Modifier" />
-  </p>
-</form>
+    <section class="col-sm-8 col-md-12">
+        <form class="well" action="" method="post">
+            <legend>
+                <h2>Modifier un commentaire</h2>
+            </legend>
+            <fieldset>
+                <?= $form ?>
+                <button class="btn" type="submit"><span class="glyphicon glyphicon-ok-sign"></span>VALIDER</button>
+            </fieldset>
+        </form>
+    </section>
+    
+</div>

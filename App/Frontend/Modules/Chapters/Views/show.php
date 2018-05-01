3beea2a7d5b6b18f9275e182263cf01df63c8821
sebastien <sebastienfolliot@yahoo.fr>
2018-05-01 10:52:59 +0200

    Modification des conditions pour gérer les commentaires en fonction de la modération et ajout d'un script pour le parametre du setFlash()

--- App/Frontend/Modules/Chapters/Views/show.php
+++ App/Frontend/Modules/Chapters/Views/show.php
@@ -1,3 +1,12 @@
+<!-- script permettant d'avoir d'afficher le message temporairement -->
+<script>
+    document.getElementById("info_comment").innerHTML = "Le commentaire a bien été ajouté !";
+
+    setTimeout(function() {
+        document.getElementById("info_comment").innerHTML = "";
+    }, 3000);
+
+</script>
 
 <div class="container" style="padding-bottom: 200px">
     <h2 style="text-align: center">
@@ -18,7 +27,7 @@
     <hr />
     <h3 style="text-align: center">Vos commentaires sur ce chapitre</h3>
     <br />
-    
+
     <?php
     if (empty($comments))
     {
@@ -29,37 +38,41 @@
 
     foreach ($comments as $comment)
     {
- 
+
     ?>
-        
+
             <fieldset>
                 <legend>
                     Posté par <strong><?= htmlspecialchars($comment['auteur']) ?></strong> le
                     <?= $comment['date']->format('d/m/Y à H\hi') ?>
-                        <?php if ($user->isAuthenticated()) { ?> -
-                        <a href="admin/comment-update-<?= $comment['id'] ?>.html">Modérer</a> |
-                        <a href="admin/comment-delete-<?= $comment['id'] ?>.html">Supprimer</a>
+                        <?php if ($user->isAuthenticated()) { ?>
+                        <?php if ($comment['moderate'] == 0) { ?>
+                        <a href="/admin/comment-moderate-<?= $comment['id'] ?>.html">  - Modérer</a>
+                        <?php } ?>
+                        <?php if ($comment['report'] == 1 && $comment['moderate'] == 0) { ?>
+                        <a href="/admin/comment-delete-report-<?= $comment['id'] ?>.html">|| Annuler la modération</a>
+                        <?php } ?>
                         <?php } ?>
                 </legend>
-                <p style="margin-bottom: 20px; <?php if ($comment['report'] == 1) { ?> color: rgba(218,218,218,0.6) <?php } else { ?> color: black <?php }?>">
-                    <?= nl2br(htmlspecialchars($comment['contenu'])) ?>
+                <p style="margin-bottom: 20px">
+                    <?php if ($comment['moderate'] == 1) { ?>
+                    <em style="color: grey">Ce commentaire a été modéré</em>
+                    <?php } else {
+                    echo nl2br(htmlspecialchars($comment['contenu'])); } ?>
                 </p>
-                <?php if ($user->isAuthenticated()) { ?>
-                <?php }
-                elseif ($comment['report'] == 1) { ?>
+                <?php 
+                if ($user->isAuthenticated()) { ?>
+                <?php } elseif ($comment['report'] == 1 && $comment['moderate'] == 0) { ?>
                 <button type="button" disabled name="comment-report" style="margin-bottom: 20px" class="btn btn-secondary btn-sm">Commentaire signalé auprès du modérateur</button>
-                <?php } 
-                else { ?>
-                <a href="/comment-reporting-<?= $comment['id'] ?>.html"><input type="button" name="comment-report" value="Signaler le commentaire" style="margin-bottom: 20px" class="btn btn-warning btn-sm" /></a>
+                <?php 
+                } elseif ($comment['report'] == 0 && $comment['moderate'] == 0) { ?>
+                <a href="/comment-reporting-<?= $comment['id'] ?>.html"><input type="button" name="comment-report" value="Signaler le commentaire" style="margin-bottom: 40px" class="btn btn-warning btn-sm" /></a>
                 <?php } ?>
-                <?php if (($user->isAuthenticated()) && $comment['report'] == 1) { ?>
+                <?php if (($user->isAuthenticated()) && $comment['report'] == 1 && $comment['moderate'] == 0) { ?>
                 <p style="color: red">Ce commentaire a été signalé</p>
                 <?php } ?>
             </fieldset>
-            <?php
-            }
-            if ($user->isAuthenticated()) {
-                
+            <?php } if ($user->isAuthenticated()) {            
             } else { ?>
             <hr />
             <p style="text-align: center"><a href="commenter-<?= $chapters['id'] ?>.html"><button class="btn btn-primary btn-lg"><i class="fas fa-comment-alt"></i><br />Ajouter un commentaire</button></a></p>

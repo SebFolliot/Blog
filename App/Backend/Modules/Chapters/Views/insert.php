88d6117506497c60c97fc3d4641975f7e395ae43
sebastien <sebastienfolliot@yahoo.fr>
2018-05-03 18:12:15 +0200

    Suppression de la class col-sm à la balise section

--- App/Backend/Modules/Chapters/Views/insert.php
+++ App/Backend/Modules/Chapters/Views/insert.php
@@ -1,5 +1,5 @@
 <div class="container">
-    <section class="col-sm-8 col-md-12">
+    <section class="col-md-12">
         <form class="well" action="" method="post">
             <legend>
                 <h2>Ajouter un chapitre</h2>

eb69165de9b471e8272acd923a3de0bb74355db3
sebastien <sebastienfolliot@yahoo.fr>
2018-05-03 10:53:29 +0200

    Configuration du host pour la connexion à la base de donnée Mysql

--- lib/Fram/PDOFactory.php
+++ lib/Fram/PDOFactory.php
@@ -5,7 +5,7 @@ class PDOFactory
 {
   public static function getMysqlConnexion()
   {
-    $db = new \PDO('mysql:host=localhost;dbname=projet_blog', 'root', '');
+    $db = new \PDO('mysql:host=db736629356.db.1and1.com;dbname=db736629356', 'dbo736629356', 'BlogJF2018!');
     $db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
  
     return $db;

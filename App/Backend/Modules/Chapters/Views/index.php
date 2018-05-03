b139916caf77075d97beae7dad9d5c91be305808
sebastien <sebastienfolliot@yahoo.fr>
2018-05-03 18:10:16 +0200

    Ajout d'un id aux balises h2 et table

--- App/Backend/Modules/Chapters/Views/index.php
+++ App/Backend/Modules/Chapters/Views/index.php
@@ -31,10 +31,10 @@
     }
 </script>
 
-<table class="table table-striped table-condensed">
-    
+<table id="chapter_table" class="table table-striped table-condensed">
+
     <div class="card-header" style="background-color:silver; color:white">
-        <h2 class="card-title" style="text-align: center">
+        <h2 id="table_title" class="card-title" style="text-align: center">
             <?php if ($nombreChapters > 0) { 
                     echo 'Gestion des ' .$nombreChapters. ' chapitres';
                   }
@@ -46,8 +46,8 @@
         <tr>
             <th>Auteur</th>
             <th>Titre</th>
-            <th>Date d'ajout</th>
-            <th>Dernière modification</th>
+            <th>Date de création</th>
+            <th class="date_modif">Dernière modification</th>
             <th>Action</th>
         </tr>
     </thead>
@@ -56,9 +56,10 @@
         <?php
         foreach ($listeChapters as $chapters)
         {
-          echo '<tr><td>', $chapters['auteur'], '</td><td>', $chapters['titre'], '</td><td>', $chapters['dateAjout']->format('d/m/Y à H\hi'), '</td><td>', ($chapters['dateAjout'] == $chapters['dateModif'] ? '-' :  $chapters['dateModif']->format('d/m/Y à H\hi')), '</td><td><a href="chapters-update-', $chapters['id'], '.html"><i class="fas fa-edit" title="Modifier"></i></a> <a onClick="delete_chapter(', $chapters['id'],')"<i class="fas fa-eraser" title="Supprimer"></i></a></td></tr>', "\n";
-        }
+          echo '<tr><td>', $chapters['auteur'], '</td><td>', $chapters['titre'], '</td><td>', $chapters['dateAjout']->format('d/m/Y à H\hi'), '</td><td class="date_modif>', ($chapters['dateAjout'] == $chapters['dateModif'] ? '-' :  $chapters['dateModif']->format('d/m/Y à H\hi')), '</td><td><a href="chapters-update-', $chapters['id'], '.html"><i class="fas fa-edit" title="Modifier"></i></a><a onClick="delete_chapter(', $chapters['id'],')"<i class="fas fa-eraser" title="Supprimer"></i></a></td></tr>', "\n";
+            
+         }
         ?>
     </tbody>
-    
+
 </table>

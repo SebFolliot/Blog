e95cbffb71593233b3ffd746934acb2e742acf89
sebastien <sebastienfolliot@yahoo.fr>
2018-05-04 19:32:59 +0200

    Suppression du style pour le footer

--- App/Backend/Templates/layout.php
+++ App/Backend/Templates/layout.php
@@ -1,10 +1,4 @@
 <?php require __DIR__.'/../../../App/Frontend/Templates/layout.php'; ?>
 
-<style>
-    /* masque le footer sur l'IHM admin */
-    footer {
-        display: none;
-    }
-</style>
     
 

273f9f73456a6ede9c55a6992acaf6820320965d
sebastien <sebastienfolliot@yahoo.fr>
2018-05-04 17:48:34 +0200

    Suppression du charset

--- lib/Fram/PDOFactory.php
+++ lib/Fram/PDOFactory.php
@@ -5,7 +5,7 @@ class PDOFactory
 {
   public static function getMysqlConnexion()
   {
-    $db = new \PDO('mysql:host=db736629356.db.1and1.com;dbname=db736629356;charset=utf8', 'dbo736629356', 'BlogJF2018!');
+    $db = new \PDO('mysql:host=db736629356.db.1and1.com;dbname=db736629356', 'dbo736629356', 'BlogJF2018!');
     $db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
  
     return $db;
